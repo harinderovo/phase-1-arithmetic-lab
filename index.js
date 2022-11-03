822b154e152f107decadce3b4e818f6dd848fe35
Harinder Singh <harmiglany@yahoo.com>
2022-11-03 00:45:44 -0700

    commit tree 2421f81a9fded19fc92b75d32d59975901d5b2f7

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
+let num1 = 31 
+let num2 = 2
 // Write your code here
+let multiply = num1 * num2 
+
+let random = 10 
+function getRandom() {
+    return Math.random();
+}
+
+let num3 = 40 
+let num4 = 10
+let mod = num3 / num4
+
+let max = 20
+function math(max) {
+    return math.max(20);
+}
